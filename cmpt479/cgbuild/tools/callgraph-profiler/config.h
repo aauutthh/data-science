098707b33ccd8250c0a5c05ad7087c6ac6ba38c3
aptmac <apmacdon@sfu.ca>
2017-09-22 12:11:50 -0700

    cmpt 363 & 479: deleted folders & files, these assignments will be created/stored elsewhere

--- cmpt479/cgbuild/tools/callgraph-profiler/config.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef CALLPROFILER_CONFIG_H
-#define CALLPROFILER_CONFIG_H
-
-#define RUNTIME_LIB "callgraph-profiler-rt"
-#define CMAKE_TEMP_LIBRARY_PATH "/home/almac/workspace/sfu-fall2017-assignments/cmpt479/cgbuild/lib"
-
-#endif
