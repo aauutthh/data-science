cf9fcfb816919a5f530ffe2dc41f91b1a2562978
aptmac <apmacdon@sfu.ca>
2017-10-12 22:36:41 -0700

    cmpt 318: e5 - finished e5

--- cmpt318/e5/reddit_weekends.py
+++ cmpt318/e5/reddit_weekends.py
@@ -13,7 +13,7 @@ import gzip
 import numpy as np
 import pandas as pd
 from scipy import stats
-
+# import matplotlib.pyplot as plt
 
 OUTPUT_TEMPLATE = (
     "Initial (invalid) T-test p-value: {initial_ttest_p:.3g}\n"
@@ -27,6 +27,13 @@ OUTPUT_TEMPLATE = (
     "Mann–Whitney U-test p-value: {utest_p:.3g}"
 )
 
+# Perform the normal tests, levene test, and ttest
+def perform_tests(weekends, weekdays):
+    weekends_normaltest = stats.normaltest(weekends['comment_count'])
+    weekdays_normaltest = stats.normaltest(weekdays['comment_count'])
+    levene = stats.levene(weekends['comment_count'], weekdays['comment_count'])
+    ttest = stats.ttest_ind(weekends['comment_count'], weekdays['comment_count'])
+    return weekends_normaltest, weekdays_normaltest, levene, ttest
 
 def main():
     # Read the gzipped file with the built in Pandas funciton
@@ -44,34 +51,59 @@ def main():
 
     # Separate the weekdays from the weekends
     # Credit to ggbaker for the hint about checking datetime.date.weekday
-    weekends = reddit[(reddit['date'].dt.weekday >= 5)] # 5 || 6 is a weekend
-    weekdays = reddit[(reddit['date'].dt.weekday < 5)] # < 5 is a weekday
+    weekends = reddit[(reddit['date'].dt.weekday >= 5)].reset_index() # 5 || 6 is a weekend
+    weekdays = reddit[(reddit['date'].dt.weekday < 5)].reset_index() # < 5 is a weekday
 
     # Student's T-test
-    initial_weekends_normaltest = stats.normaltest(weekends['comment_count'])
-    initial_weekdays_normaltest = stats.normaltest(weekdays['comment_count'])
-    initial_levene = stats.levene(weekends['comment_count'], weekdays['comment_count'])
-    initial_ttest = stats.ttest_ind(weekends['comment_count'], weekdays['comment_count'])
+    initial_weekends_normaltest, initial_weekdays_normaltest, initial_levene, initial_ttest = perform_tests(weekends, weekdays)
 
     # Fix 1: transforming data might save us.
+    # Create a copy of the dataframes, and apply a log transformation to it
+    weekends_transformed = weekends.copy(deep=True)
+    weekdays_transformed = weekdays.copy(deep=True)
+    weekends_transformed['comment_count'] = np.log(weekends_transformed['comment_count'])
+    weekdays_transformed['comment_count'] = np.log(weekdays_transformed['comment_count'])
+    transformed_weekends_normaltest, transformed_weekdays_normaltest, transformed_levene, transformed_ttest = perform_tests(weekends_transformed, weekdays_transformed)
+    # Used to show how well the np.log() normalized the data
+    # plt.figure()
+    # plt.hist(weekends_transformed['comment_count'])
+    # plt.show()
 
     # Fix 2: the Central Limit Theorem might save us.
-    
+    # Combine all weekdays and weekend days from each year/week pair and take the mean of their non-transformed count
+    # Add a week number and year column to the dataframes 
+    weekends['week'] = weekends['date'].dt.week
+    weekends['year'] = weekends['date'].dt.year
+    weekdays['week'] = weekends['date'].dt.week
+    weekdays['year'] = weekends['date'].dt.year
+
+    # Group by the values, and take the mean
+    mean_weekends = weekends.groupby(['week', 'year']).mean().reset_index()
+    mean_weekdays = weekdays.groupby(['week', 'year']).mean().reset_index()
+    mean_weekends.drop('index', axis=1, inplace=True)
+    mean_weekends.groupby(mean_weekends.week).sum()
+    mean_weekdays.drop('index', axis=1, inplace=True)
+    # print(mean_weekends['comment_count'].sum())
+    # print(mean_weekdays['comment_count'].sum())
+    weekly_weekends_normaltest, weekly_weekdays_normaltest, weekly_levene, weekly_ttest = perform_tests(mean_weekends, mean_weekdays)
+
     # Fix 3: a non-parametric test might save us.
+    # Use the Mann-Whitney test from scipy
+    mann_whitney = stats.mannwhitneyu(weekends['comment_count'], weekdays['comment_count'])
 
     print(OUTPUT_TEMPLATE.format(
         initial_ttest_p=initial_ttest.pvalue,
         initial_weekday_normality_p=initial_weekdays_normaltest.pvalue,
         initial_weekend_normality_p=initial_weekends_normaltest.pvalue,
         initial_levene_p=initial_levene.pvalue,
-        transformed_weekday_normality_p=0,
-        transformed_weekend_normality_p=0,
-        transformed_levene_p=0,
-        weekly_weekday_normality_p=0,
-        weekly_weekend_normality_p=0,
-        weekly_levene_p=0,
-        weekly_ttest_p=0,
-        utest_p=0,
+        transformed_weekday_normality_p=transformed_weekdays_normaltest.pvalue,
+        transformed_weekend_normality_p=transformed_weekends_normaltest.pvalue,
+        transformed_levene_p=transformed_levene.pvalue,
+        weekly_weekday_normality_p=weekly_weekdays_normaltest.pvalue,
+        weekly_weekend_normality_p=weekly_weekends_normaltest.pvalue,
+        weekly_levene_p=weekly_levene.pvalue,
+        weekly_ttest_p=weekly_ttest.pvalue,
+        utest_p=mann_whitney.pvalue,
     ))
 
 
