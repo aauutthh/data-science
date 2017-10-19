e3d70686ae099fc530c2e1c7e41e69aeadde4e2c
aptmac <apmacdon@sfu.ca>
2017-10-19 02:17:27 -0700

    cmpt 318: e6 - finished create_data.py and generated a data.csv

--- cmpt318/e6/analyse_data.py
+++ cmpt318/e6/analyse_data.py
@@ -6,3 +6,8 @@
 # ID#301272281
 # October 20, 2017
 
+# Reads the data.csv produced and does the relevant analysis
+# It should print the information used to answer question 3, but no specific format
+
+# Will need a statistical test that can be used to determine if the means of
+# multiple samples are different - TUKEY (& ANOVA)
