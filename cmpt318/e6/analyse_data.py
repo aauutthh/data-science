a66fd0902e4e22a03aa79cbf6c8072b22f8ad02b
aptmac <apmacdon@sfu.ca>
2017-10-20 20:15:05 -0700

    cmpt 318: e6 - finished e6

--- cmpt318/e6/analyse_data.py
+++ cmpt318/e6/analyse_data.py
@@ -20,6 +20,13 @@ from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 data = pd.read_csv('data.csv')
 
+# Based on the above figure, I did some quick analysis to show the mean values
+# for the significantly different sorts, as determined by Tukey
+# QS1, Partition Sort, QS4/5, QS2/3, Mergesort
+print("Here are the raw means for each search.")
+print("Use with below Tukey test to infer results:")
+print(data.mean().sort_values())
+
 # Code for post-hoc Tukey test borrowed from:
 # http://www.cs.sfu.ca/~ggbaker/data-science/content/stats-tests.html
 melt = pd.melt(data)
@@ -31,12 +38,5 @@ posthoc = pairwise_tukeyhsd(
 print(posthoc)
 
 # I used the figure that is commentet out below to answer #3
-fig = posthoc.plot_simultaneous()
-plt.show()
-
-# Based on the above figure, I did some quick analysis to show the mean values
-# for the significantly different sorts, as determined by Tukey
-
-# TODO: Create a simple function that calculates the mean of each row and outputs them
-# only for those which are different from each other
-# QS1, Partition Sort, QS4/5, QS2/3, Mergesort
+# fig = posthoc.plot_simultaneous()
+# plt.show()
